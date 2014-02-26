611167ec2d936d8a90b7a6c5d501661b1b62747b
anderjoc <jocelynn.anderson@gmail.com>
2014-02-25 17:57:18 -0700

    slider

--- js/picture.js
+++ js/picture.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-$("#my-slider").slippity({
-arrowClass: ".arrow", // the selector for arrows navigation.
-slideClass: ".slide", // the selector for slide items.
-dynamicHeight: true, // dynamiclly set the height of your images
-animationTime: 500, // the delay time between slides. 
-start: function(){}, // callback
-end: function(){}}); // callback
+	$("#my-slider").slippity({
+	arrowClass: ".arrow", // the selector for arrows navigation.
+	slideClass: ".slide", // the selector for slide items.
+	dynamicHeight: true, // dynamiclly set the height of your images
+	animationTime: 500, // the delay time between slides. 
+	start: function(){}, // callback
+	end: function(){}}); // callback
 });
